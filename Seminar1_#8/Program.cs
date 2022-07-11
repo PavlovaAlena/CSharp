e549eb063b7cb3e2ea4f53cf0cc2eef14aba0cd7
Alena <al_lucky@mail.ru>
2022-07-11 15:01:43 +0400

    Solution of the 4 task

---
+++ Seminar1_#8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число, для вывода четных чисел от 1 до N: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > 1)
+{
+    for (int i = 2; i <= num1; i += 2)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine($"- четныe числа от 1 до {num1}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} меньше первого четного числа 2");
+}
