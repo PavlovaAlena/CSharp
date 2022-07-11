40fad6803705849f9b69199effeef6b33dc4fafe
Alena <al_lucky@mail.ru>
2022-07-11 10:49:20 +0400

    Solving the first task

---
+++ Seminar1_#2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите 1-ое число для сравнения: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число для сравнения: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 )
+{
+    Console.WriteLine($"Первое число {num1} больше второго {num2}");
+}
+else if (num1 < num2 )
+{
+    Console.WriteLine($"Второе число {num2} больше первого {num1}" );
+}
+else
+{
+    Console.WriteLine($"Числа {num1} и {num2} равны" );
+}
+
