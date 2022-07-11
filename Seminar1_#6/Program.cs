635a005cce974bfe6c4fa521b17372f8922d7ed2
Alena <al_lucky@mail.ru>
2022-07-11 13:24:19 +0400

    Solution of the third task

---
+++ Seminar1_#6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число, проверяемое на четность: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 %2 == 0)
+{
+    Console.WriteLine($"Число {num1} четное");
+}
+else
+{
+    Console.WriteLine($"Число {num1} нечетное" );
+}
